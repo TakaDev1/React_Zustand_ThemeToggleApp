0e0f22b46fe749f83b703201c23d9aca6a670dc3
Izumi Takayuki <r8quattrospider@gmail.com>
2026-08-20 21:31:35 +0900

    style: update global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 
 import './App.css'
-import Theme from './components/Theme'
+import Theme from './components/Theme';
+import { useThemeStore } from './stores/ThemeStore'
 
 function App() {
-  
+  const {darkMode} = useThemeStore();
 
   return (
     <>
-    <h1>ThemeToggleApp</h1>
-    <Theme />
+      <div className={`min-h-screen flex flex-col items-center justify-center ${darkMode ? '' : 'bg-gray-400 text-black'}`}>
+        <h1 className='!mb-20'>ThemeToggleApp</h1>
+        <Theme />
+      </div>
     </>
   )
 }
